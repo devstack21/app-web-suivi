e5bd4da603b4563d89ad09b756711b8990598f04
Joanna Tchokonte <joanna.tchokonte@iccsoft.cm>
2023-11-27 16:14:58 +0100

    added accounts reducer

--- src/store/reducers/Accounts/accountsReducer.js
+++ src/store/reducers/Accounts/accountsReducer.js
@@ -3,7 +3,6 @@ import { combineReducers } from 'redux';
 import listReducer from './listSlice';
 import createSlice from './createSlice';
 import editSlice from './editSlice';
-// Import other reducers...
 
 const accountsReducer = combineReducers({
   create: createSlice,
